b4d65d6cb5b5d7dbb0fb4d82ee1260804e734419
Daniher01 <daniher02@gmail.com>
2024-12-11 16:08:36 -0300

    se actualiza el .py de estadistica interna

--- querys/estadistica_interna.py
+++ querys/estadistica_interna.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 def convertir_csv_a_sql_estadistica_interna():
     """
-    Convierte los datos del CSV Estadistica_interna.csv a SQL generando un nuevo archivo .sql
+    Convierte los datos del CSV Estadistica_Interna.csv a SQL generando un nuevo archivo .sql,
+    incluyendo las nuevas columnas jugador_id, partido_id, y competicion_temporada_id.
     """
     input_path = 'archivos csv/Estadistica_Interna.csv'  # Ruta al CSV de entrada
     output_path = 'archivos sql/estadistica_interna.sql'  # Ruta al archivo SQL de salida
@@ -10,11 +11,11 @@ def convertir_csv_a_sql_estadistica_interna():
 
     # Leer el archivo CSV
     df = pd.read_csv(input_path, sep=separador)
-    # df = df[(df['IdEstadisticaPartido'] >= 14222) & (df['IdEstadisticaPartido'] < 16000)]
 
     # Reemplazar NaN o valores vacíos con 0
     df = df.fillna(0)
 
+    # Lista para almacenar las consultas generadas
     lista_query = []
 
     for i in df.index:
@@ -25,7 +26,9 @@ def convertir_csv_a_sql_estadistica_interna():
                 goles, 
                 amarillas, 
                 rojas, 
-                citaciones_id
+                jugador_id,
+                partido_id,
+                competicion_temporada_id
             ) 
             VALUES (
                 {df['IdEstadisticaPartido'][i] if df['IdEstadisticaPartido'][i] != 0 else 'DEFAULT'},
@@ -33,6 +36,8 @@ def convertir_csv_a_sql_estadistica_interna():
                 {df['Goles'][i]},
                 {df['Amarillas'][i]},
                 {df['Rojas'][i]},
+                {df['IdJugador'][i]},
+                {df['IdPartido'][i]},
                 {df['IdCT'][i]}
             )
             ON CONFLICT (id_estadistica) DO UPDATE
@@ -41,7 +46,9 @@ def convertir_csv_a_sql_estadistica_interna():
                 goles = EXCLUDED.goles,
                 amarillas = EXCLUDED.amarillas,
                 rojas = EXCLUDED.rojas,
-                citaciones_id = EXCLUDED.citaciones_id;
+                jugador_id = EXCLUDED.jugador_id,
+                partido_id = EXCLUDED.partido_id,
+                competicion_temporada_id = EXCLUDED.competicion_temporada_id;
         """
         lista_query.append(query)
 
@@ -51,3 +58,4 @@ def convertir_csv_a_sql_estadistica_interna():
             f.write(query + '\n')
 
     print('Archivo convertido con éxito en:', output_path)
+
